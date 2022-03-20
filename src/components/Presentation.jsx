e680940b1bf94474e808757d160a377e2b724799
Deramecourt Mathurin <contact@mderam.com>
2022-03-20 18:16:35 +0100

    fix(style): background images display properly on all screen size

--- src/components/Presentation.jsx
+++ src/components/Presentation.jsx
@@ -1,7 +1,7 @@
 function Presentation() {
     return (
         <div className="project">
-            <div className="background" style={{ backgroundImage: `url(${process.env.PUBLIC_URL}/img/presentation.png)` }}></div>
+            <div className="background bgLeft" style={{ backgroundImage: `url(${process.env.PUBLIC_URL}/img/presentation.png)` }}></div>
             <div className="presentation">
                 <h1>Hello, I'm Mathurin.<br />
                 I develop applications.</h1>

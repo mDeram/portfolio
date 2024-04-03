4b61531b22b72cd75b7decb979dab1d74e1a7a24
Deramecourt Mathurin <contact@mderam.com>
2024-04-03 09:31:21 +0200

    feat: improved content & styles

--- src/App.jsx
+++ src/App.jsx
@@ -8,15 +8,15 @@ import { SiGithub, SiMaildotru } from "react-icons/si";
 
 function Project({ src, title, href, content }) {
     return (
-        <div className="flex flex-col gap-16 border border-blue-800/50 rounded-2xl bg-blue-200/50">
-            <div className="rounded-2xl p-8 flex text-neutral-900">
-                <div className="w-1/3">
+        <div className="flex flex-col gap-16 border border-blue-800/50 rounded-2xl bg-purple-200/50">
+            <div className="rounded-2xl p-8 flex flex-col gap-8 sm:gap-0 sm:flex-row text-neutral-900">
+                <div className="w-full sm:w-1/3 mr-4">
                     <a href={href} target="_blank" className="hover:text-purple-500 transition-all"><h3 className="font-medium text-xl" rel="noopener noreferrer">{title}</h3></a>
-                    <div className="text-neutral-500 pl-4">
+                    <div className="text-neutral-500 pl-2">
                         {content}
                     </div>
                 </div>
-                <img src={src} className="w-2/3 rounded-2xl magic-border" />
+                <img src={src} className="w-full sm:w-2/3 rounded-2xl magic-border aspect-video" />
             </div>
         </div>
     )
@@ -37,14 +37,16 @@ function App() {
             <section>
                 <h2 className="text-xl text-neutral-900 font-medium mb-4 underline decoration-2 decoration-blue-500">About</h2>
                 <p className="text-gray-600">
-                    I love logic and satisfaction brough by well built systems. I have an interest in infrastructure architecture. I'm curious, autonomous and a team player.
+                    Hello, I'm Mathurin.
                     <br /><br />
                     Back in 2015, curious about computers automation capabilities, I started creating web applications, later I had fun with 2D game development. Fast-forward to today, I've built multiple
                     applications for myself, friends and a start-up.
                     <br /><br />
+                    I love logic and satisfaction brough by well built systems. I have an interest in infrastructure architecture. I'm a curious autonomous team player.
+                    <br /><br />
                     Nowadays I'm focused on building reactive and reliable systems with an interest for efficiency, I'm currently learning Go.
                     <br /><br />
-                    When I'm not at a computer, I'm usually doing gymnastics, playing piano, hanging out with my wife or solving rubik's cubes.
+                    When I'm not at a computer, I'm usually doing gymnastics, playing piano, hanging out with my friends or solving rubik's cubes.
                 </p>
             </section>
             <section className="mt-16">
@@ -59,10 +61,10 @@ function App() {
                         <div className="pl-9 text-neutral-500">
                             <p>DUALEAP - Connected aircrafts</p>
                             <p className="mt-4">
-                                Worked with system developers to handle incoming data.
+                                In that company I did the following: Worked with system developers to handle incoming data.
                                 Built a CD/CI pipeline.
                                 Along with databases management I improved existing schemas.
-                                Designed and implemented web interfaces with React.
+                                Designed and implemented web interfaces with React while integrating a 3D environment.
                                 Worked on user authentication, authorization and management, multiple APIs, billing reports, 3D visualization.
                                 Optimized performances of the existing code and architecture.
                             </p>
@@ -73,14 +75,14 @@ function App() {
             <section className="mt-16">
                 <h2 className="text-xl text-neutral-900 font-medium mb-8 underline decoration-2 decoration-purple-500">Projects</h2>
                 <div className="flex flex-col gap-16">
-                    <Project title="Drive" src="./img/drive_index.png" href="https://github.com/mDeram/cloud-frontend" content={
-                        <p>A simple drive</p>
+                    <Project title="Drive" src="./portfolio/img/drive_index.png" href="https://github.com/mDeram/cloud-frontend" content={
+                        <p>Upload, store and download files from everywhere with an internet connection.</p>
                     } />
-                    <Project title="Multiplayer Sudoku" src="./img/sudoku.png" href="https://github.com/mDeram/sudoku-frontend" content={
-                        <p>A multiplayer sudoku!</p>
+                    <Project title="Multiplayer Sudoku" src="./portfolio/img/sudoku.png" href="https://github.com/mDeram/sudoku-frontend" content={
+                        <p>Choose difficulty, share a link with one or more friends and as soon as they join the game start. You colaboratively solving a sudoku.</p>
                     } />
-                    <Project title="2D Map Editor" src="./img/2d_map_editor.png" href="https://github.com/mDeram/2DMapEditor" content={
-                        <p>A 2D map editor</p>
+                    <Project title="2D Map Editor" src="./portfolio/img/2d_map_editor.png" href="https://github.com/mDeram/2DMapEditor" content={
+                        <p>A simple map editor to help game developers with their map building.</p>
                     } />
                 </div>
             </section>
